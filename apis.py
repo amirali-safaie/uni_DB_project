8d277839eee4803b98a97c5a1719b2e2cca1e9dd
amirali <amirali.safie@gamil.com>
2024-06-10 22:03:34 +0330

    load password from environment varialbe

--- apis.py
+++ apis.py
@@ -1,10 +1,18 @@
 import mysql.connector
- 
+import os
+from dotenv import load_dotenv
+
+
+#import pass from env variable
+load_dotenv()
+mysql_password = os.getenv('mysql_password')
+print(mysql_password)
+
 # Creating connection object
 mydb = mysql.connector.connect(
     host = "localhost",
     user = "root",
-    password = "mysql_password"
+    password = mysql_password
 )
 
 cursor = mydb.cursor() # create an instance of cursor class to execute mysql commands
