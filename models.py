eaa774754ab6d5aeb31104657bb7a8855c2619f6
amirali <amirali.safie@gamil.com>
2024-06-19 12:39:26 +0330

    update advertise model and add type_num to it

--- models.py
+++ models.py
@@ -3,11 +3,12 @@ from datetime import datetime, timedelta
 from typing import Optional, List
 
 class Advertise(BaseModel):
-    price: Optional[float] = Field(None, title="Price associated with the advertisement")
-    description: Optional[str] = Field(None, title="Description of the advertisement", max_length=200)
+    price: Optional[float] = Field(..., title="Price associated with the advertisement")
+    description: Optional[str] = Field(..., title="Description of the advertisement", max_length=200)
     title: str = Field(..., title="Title of the advertisement", max_length=100)
-    phone_number: Optional[str] = Field(None, title="Contact phone number for the advertisement", max_length=11)
-    city_id: Optional[int] = Field(None, title="ID of city related to the advertisement")
+    phone_number: Optional[str] = Field(..., title="Contact phone number for the advertisement", max_length=11)
+    city_id: Optional[int] = Field(..., title="ID of city related to the advertisement")
+    type_name: str = Field(..., title="Type of the advertisement")
 
 # You may need to adjust types and constraints as per your actual database schema.
 
