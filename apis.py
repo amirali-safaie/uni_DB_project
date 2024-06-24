73738740668fc84fb32d3197a54c691539fd0aa4
Amirali <amiralihabibi82@gmail.com>
2024-06-24 23:24:39 +0330

    Add feature for Admin to see all reports about advertises

--- apis.py
+++ apis.py
@@ -7,7 +7,7 @@ from fastapi import FastAPI, Query, Path, Body, Header, HTTPException
 from pydantic import BaseModel, Field
 from typing import Optional, Union, Tuple, Annotated, List
 #...import fastapi packages.......
-from models import Advertise, UserProfileUpdate, AdReport, AdReview
+from models import   AdReview
 
 # Import pass from env variable
 load_dotenv()
@@ -46,3 +46,27 @@ async def review_advertise(advertise_id: int, review: AdReview):
     
     return {"message": f"Advertise ID {advertise_id} has been updated to status '{review.status}'"}
 
+@app.get("/support/reports")
+async def get_ad_reports():
+    cursor.execute("""
+        SELECT r.report_id, r.writer_id, r.Moderator_id, r.advertise_id, r.note, r.status, rc.name AS type_name
+        FROM report r
+        INNER JOIN report_category rc ON rc.cat_id = r.type
+        ORDER BY r.report_id DESC
+    """)
+    
+    reports = cursor.fetchall()
+    
+    response = []
+    for report in reports:
+        response.append({
+            "report_id": report[0],
+            "writer_id": report[1],
+            "moderator_id": report[2],
+            "advertise_id": report[3],
+            "note": report[4],
+            "status": report[5],
+            "type_name": report[6]
+        })
+    
+    return response
--- models.py
+++ models.py
@@ -2,6 +2,6 @@ from pydantic import BaseModel, Field
 from datetime import datetime, timedelta
 from typing import Optional, List
 
-
+    
 class AdReview(BaseModel):
     status: str = Field(..., title="Status of the advertisement ('accepted' or 'rejected')")
